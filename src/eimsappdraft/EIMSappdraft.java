0d27003d63c63cf7734c4322b5f84a0f0ab43088
MarcellLeath <celly12343@gmail.com>
2013-10-29 00:27:58 -0700

    Fixed Formatting

--- src/eimsappdraft/EIMSappdraft.java
+++ src/eimsappdraft/EIMSappdraft.java
@@ -2,32 +2,25 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package eimsappdraft;
+
 import javax.swing.JOptionPane;
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 
-
-
 public class EIMSappdraft {
-    
-public class Welcome implements ItemListener {
-    JPanel cards; //a panel that uses CardLayout
-    final static String BUTTONPANEL = "Card with JButtons";
-    final static String TEXTPANEL = "Card with JTextField";
-    
-    /*JOptionPane.showMessageDialog(null, "<html><br> Hello Welcome to EIMS,  "
-            + "<br> Your group is being populated ");
-            */
 
-    
+    public class Welcome implements ItemListener {
 
+        JPanel cards; //a panel that uses CardLayout
+        final static String BUTTONPANEL = "Card with JButtons";
+        final static String TEXTPANEL = "Card with JTextField";
 
-public static void main(String[] args) {
-
+        /*JOptionPane.showMessageDialog(null, "<html><br> Hello Welcome to EIMS,  "
+         + "<br> Your group is being populated ");
+         */
+        public static void main(String[] args) {
+        }
     }
-
-}
 }
