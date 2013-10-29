a32a6faaebb3a476fd6d3ee811c289e82947d0ae
MarcellLeath <celly12343@gmail.com>
2013-10-29 00:34:36 -0700

    I've also made a stupid change

--- src/eimsappdraft/EIMSappdraft.java
+++ src/eimsappdraft/EIMSappdraft.java
@@ -18,7 +18,7 @@ public class EIMSappdraft {
         final static String TEXTPANEL = "Card with JTextField";
 
         /* JOptionPane.showMessageDialog(null, "<html><br> Hello Welcome to EIMS,  "
-         + "<br> Your group is being populated ");
+         +      "<br> Your group is being populated ");
          */
         public static void main(String[] args) {
         }
