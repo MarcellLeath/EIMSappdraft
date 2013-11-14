df13e633d33120e6838a6d48ff799aee91d470b2
MarcellLeath <celly12343@gmail.com>
2013-11-13 22:04:46 -0800

    My Changes

--- src/eimsappdraft/EIMSappdraft.java
+++ src/eimsappdraft/EIMSappdraft.java
@@ -8,19 +8,32 @@ import javax.swing.JOptionPane;
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
+import net.miginfocom.swing.MigLayout;
 
 public class EIMSappdraft {
 
-    public class Welcome implements ItemListener {
+    JPanel cards; //a panel that uses CardLayout
+    final static String BUTTONPANEL = "Card with JButtons";
+    final static String TEXTPANEL = "Card with JTextField";
 
-        JPanel cards; //a panel that uses CardLayout
-        final static String BUTTONPANEL = "Card with JButtons";
-        final static String TEXTPANEL = "Card with JTextField";
+    /*      JOptionPane.showMessageDialog(null, "<html><br> Hello Welcome to EIMS,  "
+     + "<br> Your group is being populated ");
+     */
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("title");
+        
+        JPanel panel = new JPanel(new MigLayout());
 
-        /*      JOptionPane.showMessageDialog(null, "<html><br> Hello Welcome to EIMS,  "
-         + "<br> Your group is being populated ");
-         */
-        public static void main(String[] args) {
-        }
+        panel.setSize(400, 400);
+        panel.add(new JLabel("label"));
+        panel.add(new JTextField("Text field"));
+        panel.add(new JLabel("label"));
+        panel.add(new JTextField("It's a wrap"));
+        panel.add(new JTextArea("Write some shit here"));
+        panel.setVisible(true);
+        
+        frame.setSize(500, 500);
+        frame.add(panel);
+        frame.setVisible(true);
     }
 }
